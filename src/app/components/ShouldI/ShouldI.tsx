e91e86c7c7346da6fd0a7f51db64551acf5b325e
rifatNr <Rifatnoor92@gmail.com>
2024-04-28 04:20:37 +0600

    added some condition

--- src/app/components/ShouldI/ShouldI.tsx
+++ src/app/components/ShouldI/ShouldI.tsx
@@ -1,25 +1,32 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
 import yes from "/public/yes.png";
 import no from "/public/no.png";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 const ShouldI = () => {
+    const [isShowAns, setIsShowAns] = useState(false);
+
     const isYes = useMemo(() => Math.random() <= 0.5, []);
 
     return (
         <div className="h-screen w-screen bg-pink-100">
-            <div className="w-full pt-20">
-                <div className="text-center font-indie-flower-regular text-[10vh] font-extrabold">
+            {!isShowAns && (
+                <div
+                    onClick={() => setIsShowAns(true)}
+                    className="w-full absolute-center text-center font-indie-flower-regular text-[10vh] cursor-pointer"
+                >
                     Should I?
                 </div>
-                <div className="flex justify-center items-center mt-10">
-                    {isYes && (
-                        <img className="w-[40vh] h-auto" src={yes.src} alt="" />
-                    )}
-                    {!isYes && (
-                        <img className="w-[40vh] h-auto" src={no.src} alt="" />
-                    )}
-                </div>
+            )}
+            <div className="absolute-center">
+                {isShowAns && (
+                    <img
+                        className="w-[40vh] h-auto"
+                        src={isYes ? yes.src : no.src}
+                        alt=""
+                    />
+                )}
             </div>
         </div>
     );
